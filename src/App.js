4f3606474fbccb42b7370673028e71898e5d1723
vanshitaprep <133661559+vanshitaprep@users.noreply.github.com>
2024-08-27 17:21:00 +0530

    api integrated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,43 @@ import CareerApplication from './Components/SideBarComponents/CareerApplication/
 import Employee from './Components/SideBarComponents/Employee/Employee';
 import DashBoard from './Components/SideBarComponents/DashBoard/DashBoard';
 import TopBarComponent from './Components/TopBarComponent/TopBarComponent';
+import { useNavigate } from 'react-router-dom';
+import {notification} from 'antd'
+import { Button } from 'antd';
 function App() {
+  const signIn = async ()=>{
+    try {
+      const response = await fetch(`http://localhost:4040/api/users/signinV2`, {
+          method: 'POST',
+          headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              user: { email: "vanshitsa@prepseed.com", password: "a1b1#c1d1@" }
+          }),
+          credentials: 'include',
+      });
+
+      const responseJson = await response.json();
+    
+      if (!response.ok) {
+        // If response status is not 200, show an error notification
+        notification.error({
+          message: 'Sign In Failed',
+          description: 'You are not allowed to sign in here'
+        });
+        return;
+      }
+      const token = responseJson.token;
+      localStorage.setItem('token',token);
+      return responseJson;
+
+  } catch (error) {
+      throw new Error(error.message);
+  }
+
+  }
   return (
     <BrowserRouter>
       <Navigation />
@@ -17,7 +53,10 @@ function App() {
           <Route path="/CareerApplication" element={<CareerApplication />} />
           <Route path="/Employee" element={<Employee />} />
         </Routes>
+
       </div>
+      <Button onClick={signIn}>Sign in</Button>
+    
     </BrowserRouter>
   );
 }
